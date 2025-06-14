6b27bc71c60adea2fb51fdcc539587ee00109dae
Daniel Burrall <71298259+Dburrall@users.noreply.github.com>
2025-06-14 12:50:18 -0400

    added motivational quote and changed Drupal hover text color

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
+"use client";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+	const [quote, setQuote] = useState<string | null>(null); //typescript sucks and you have to tell it that it starts as null then becomes a string later
+
+	useEffect(() => {
+		fetch("https://api.quotable.io/random")
+			.then((res) => res.json())
+			.then((data) => setQuote(`${data.content} — ${data.author}`))
+			.catch((err) => setQuote("Error fetching quote."));
+	}, []);
 	return (
 		<div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16  sm:max-w-2.5 md:max-w-none font-[family-name:var(--font-geist-sans)]'>
 			<main className='flex flex-col gap-[32px] row-start-2 items-center sm:items-start'>
@@ -28,7 +38,7 @@ export default function Home() {
 					</a>
 
 					<a
-						className='rounded-full  border border-[#0077b5] transition-colors flex items-center justify-center hover:bg-[#0077b5]  dark:hover:text-black hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]'
+						className='rounded-full  border border-[#0077b5] transition-colors flex items-center justify-center hover:bg-[#0077b5]  dark:hover:text-white hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]'
 						href='/Drupal'>
 						Drupal
 					</a>
@@ -48,6 +58,12 @@ export default function Home() {
 							Coming soon!
 						</span>
 					</a>
+				</div>
+				<div className='max-w-42 sm:max-w-80 '>
+					<h3 className='border-b-2 border-amber-400 text-2xl'>
+						Daily Motivation
+					</h3>
+					{quote ? <p>{quote}</p> : <p>Loading motivational wisdom…</p>}
 				</div>
 			</main>
 			<footer className='row-start-3 flex gap-[24px] flex-wrap items-center justify-center'></footer>
